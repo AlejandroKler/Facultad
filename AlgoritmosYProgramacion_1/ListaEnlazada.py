dbf2340597b7d4f17d44e69dae949a4ee2d37d06
Alejandro <alejandro_kler@outlook.com>
2017-05-18 14:05:41 -0300

    Create ListaEnlazada.py

---
+++ AlgoritmosYProgramacion_1/ListaEnlazada.py
@@ -0,0 +1,10 @@
+class ListaEnlazada:
+    """Modela una lista enlazada."""
+
+    def __init__(self):
+        """Crea una lista enlazada vacía."""
+        # referencia al primer nodo (None si la lista está vacía)
+        self.prim = None
+        # cantidad de elementos de la lista
+        self.len = 0
+        return self
