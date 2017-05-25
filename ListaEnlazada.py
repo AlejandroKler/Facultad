d433298d80a4fb08b182350505e10237d44da58e
leobellaera <leobellaera@gmail.com>
2017-05-25 19:01:36 -0300

    Update ListaEnlazada.py

--- ListaEnlazada.py
+++ ListaEnlazada.py
@@ -33,3 +33,38 @@ class ListaEnlazada:
             n_ant.prox = n_act.prox
         self.len -= 1
         return dato
+
+    def remove(self, x):
+        """Borra la primera aparición del valor x en la lista.
+         Si x no está en la lista, levanta ValueError"""
+        if self.len == 0:
+            raise ValueError("Lista vacía")
+        if self.prim.dato == x:
+            self.prim = self.prim.prox
+        else:
+            n_ant = self.prim
+            n_act = n_ant.prox
+        while n_act is not None and n_act.dato != x:
+            n_ant = n_act
+            n_act = n_ant.prox
+        if n_act == None:
+            raise ValueError("El valor no está en la lista.")
+            n_ant.prox = n_act.prox
+        self.len -= 1
+        
+    def insert(self, i, x):
+        """Inserta el elemento x en la posición i.
+        Si la posición es inválida, levanta IndexError"""
+        if i < 0 or i > self.len:
+            raise IndexError("Posición inválida")
+        nuevo = _Nodo(x)
+        if i == 0:
+            nuevo.prox = self.prim
+            self.prim = nuevo
+        else:
+            n_ant = self.prim
+            for pos in range(1, i):
+                n_ant = n_ant.prox
+            nuevo.prox = n_ant.prox
+            n_ant.prox = nuevo
+        self.len += 1
