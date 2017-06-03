7231d80ab1c8dd2919c4d80e894291acc653fc1b
leobellaera <leobellaera@gmail.com>
2017-06-03 13:18:22 -0300

    Update MarcaDeTiempo.py

--- MarcaDeTiempo.py
+++ MarcaDeTiempo.py
@@ -1,18 +1,18 @@
 class MarcaDeTiempo():
 	"""Descripcion"""
 	def __init__(self,duracion):
-        """Crea una instancia de la clase.
-        Parametros:
+        	"""Crea una instancia de la clase.
+        	Parametros:
         	duracion (float) Duracion de la marca de tiempo"""
-        self.duracion = duracion
-        self.habilitados = []
+        	self.duracion = duracion
+        	self.habilitados = []
 
-    def habilitar_track(self,posicion):
-    	if not posicion in self.habilitados:
+	def habilitar_track(self,posicion):
+    		if not posicion in self.habilitados:
     		self.habilitados.append(posicion)
 
-    def deshabilitar_track(self,posicion):
-    	try:
-    		self.habilitados.remove(posicion)
-        except ValueError:
-            pass
+	def deshabilitar_track(self,posicion):
+    		try:
+    			self.habilitados.remove(posicion)
+        	except ValueError:
+            		pass
