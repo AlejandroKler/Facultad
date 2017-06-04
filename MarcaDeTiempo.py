0813f2ff7b3fd8af6e5eb3d379af18a4dd7c5c7d
leobellaera <leobellaera@gmail.com>
2017-06-04 20:49:10 -0300

    Update MarcaDeTiempo.py

--- MarcaDeTiempo.py
+++ MarcaDeTiempo.py
@@ -6,13 +6,3 @@ class MarcaDeTiempo():
         	duracion (float) Duracion de la marca de tiempo"""
         	self.duracion = duracion
         	self.habilitados = []
-
-	def habilitar_track(self,posicion):
-    		if not posicion in self.habilitados:
-    		self.habilitados.append(posicion)
-
-	def deshabilitar_track(self,posicion):
-    		try:
-    			self.habilitados.remove(posicion)
-        	except ValueError:
-            		pass
