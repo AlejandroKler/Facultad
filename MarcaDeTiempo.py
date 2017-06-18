b4c9b2e5baced439c9ae782c2ef2cd5770659077
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-17 22:31:34 -0300

    Listo

--- MarcaDeTiempo.py
+++ MarcaDeTiempo.py
@@ -1,8 +1,10 @@
 class MarcaDeTiempo():
-    """Descripcion"""
+    """Una marca de tiempo consiste en un conjunto de tracks que deben sonar (habilitados) y la duracion de la misma."""
     def __init__(self,duracion):
-        """Crea una instancia de la clase.
+        """
+        Crea una instancia de la clase.
         Parametros:
-        duracion (float) Duracion de la marca de tiempo"""
+            duracion (float) Duracion de la marca de tiempo
+        """
         self.duracion = duracion
-        self.habilitados = []
+        self.tracks_habilitados = []
