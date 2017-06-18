e6df8c54b0f23d4dbf33d01c0a12a18b6fa4918e
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-17 22:49:21 -0300

    Listo

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ def load(file,shell=None):
     """Carga la cancion desde el archivo. Reemplaza la cancion en edicion
     actual si es que la hay.
     Parametros:
-        file (string) Debe tente el nombre del archivo junto con su extencion (.plp)"""
+        file (string) Debe tener el nombre del archivo junto con su extencion (.plp)
+        shell (object) Objeto de la clase Shell para actualizar la cancion"""
     cancion = Cancion()
     try:
         with open(file,"r") as f:
@@ -13,9 +14,9 @@ def load(file,shell=None):
                 if campo == "S":
                     funcion,frecuencia,volumen = valor.split("|")
                     cancion.track_add(funcion,frecuencia,volumen)
-                if campo == "T":
-                    duracion = valor
-                if campo == "N":
+                elif campo == "T":
+                    duracion = float(valor)
+                elif campo == "N":
                     cancion.mark_add(duracion)
                     posicion = 0
                     for caracter in valor:
@@ -25,7 +26,7 @@ def load(file,shell=None):
     except IOError as e:
         return "I/O error({0}): {1}".format(e.errno, e.strerror)
     if shell:
-        shell.cancion = cancion # Actualizamos el atributo del objeto shell
+        shell.actualizar_cancion(cancion) # Actualizamos el atributo del objeto shell
     return "Cancion cargada con exito"
 
 Shell().cmdloop()
