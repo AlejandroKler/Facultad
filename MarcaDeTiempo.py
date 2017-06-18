086da471b1d30577e59e85d46e88b6fff2384ec1
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-17 22:26:39 -0300

    Version entregada

--- MarcaDeTiempo.py
+++ MarcaDeTiempo.py
@@ -1,8 +1,8 @@
 class MarcaDeTiempo():
-	"""Descripcion"""
-	def __init__(self,duracion):
-        	"""Crea una instancia de la clase.
-        	Parametros:
-        	duracion (float) Duracion de la marca de tiempo"""
-        	self.duracion = duracion
-        	self.habilitados = []
+    """Descripcion"""
+    def __init__(self,duracion):
+        """Crea una instancia de la clase.
+        Parametros:
+        duracion (float) Duracion de la marca de tiempo"""
+        self.duracion = duracion
+        self.habilitados = []
