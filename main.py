e5f5e90c784f29836f46f5ae24f0449145a8e773
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-17 22:25:57 -0300

    version entregada

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 from Shell import Shell
 
 def load(file,shell=None):
-	"""Carga la cancion desde el archivo. Reemplaza la cancion en edicion
+    """Carga la cancion desde el archivo. Reemplaza la cancion en edicion
     actual si es que la hay.
     Parametros:
-    	file (string) Debe tente el nombre del archivo junto con su extencion (.plp)"""
+        file (string) Debe tente el nombre del archivo junto con su extencion (.plp)"""
     cancion = Cancion()
     try:
-	    with open(file,"r") as f:
-	    	for linea in f:
-	    		campo,valor = linea.rstrip("\n").split(",")
-	    		if campo == "S":
-	    			funcion,frecuencia,volumen = valor.split("|")
-	    			cancion.track_add(funcion,frecuencia,volumen)
-	    		if campo == "T":
-	    			duracion = valor
-	    		if campo == "N":
-	    			cancion.mark_add(duracion)
-	    			posicion = 0
-	    			for caracter in valor:
-	    				if caracter == "#":
-	    					cancion.track_on(posicion)
-	    				posicion += 1
+        with open(file,"r") as f:
+            for linea in f:
+                campo,valor = linea.rstrip("\n").split(",")
+                if campo == "S":
+                    funcion,frecuencia,volumen = valor.split("|")
+                    cancion.track_add(funcion,frecuencia,volumen)
+                if campo == "T":
+                    duracion = valor
+                if campo == "N":
+                    cancion.mark_add(duracion)
+                    posicion = 0
+                    for caracter in valor:
+                        if caracter == "#":
+                            cancion.track_on(posicion)
+                        posicion += 1
     except IOError as e:
-    	return "I/O error({0}): {1}".format(e.errno, e.strerror)
+        return "I/O error({0}): {1}".format(e.errno, e.strerror)
     if shell:
-    	shell.cancion = cancion # Actualizamos el atributo del objeto shell
-	return "Cancion cargada con exito"
+        shell.cancion = cancion # Actualizamos el atributo del objeto shell
+    return "Cancion cargada con exito"
 
 Shell().cmdloop()
