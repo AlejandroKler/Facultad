9735d90225599696dd7fa634794b057d6d4c509a
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-18 23:37:42 -0300

    Update Shell.py

--- Shell.py
+++ Shell.py
@@ -2,21 +2,12 @@ from Cancion import Cancion
 from ListaEnlazada import ListaEnlazada,_IteradorListaEnlazada
 import cmd
 
-def convert_num(cadena,isfloat=False):
-    """ Convierte una cadena en un int o float segun corresponda. Imprime un mensaje de error si no recibe un numero"""
-    if not cadena.isdigit():
-        print("Ha ingresado un parametro incorrectamente")
-        return
-    if isfloat:
-        return float(cadena)
-    return int(cadena)
-
 class Shell(cmd.Cmd):
     intro = "Bienvenido a Sounds of Cyber City.\n Ingrese help o ? para listar los comandos.\n"
     prompt = "*>> "
     cancion = Cancion()
     def do_LOAD(self,file):
-        print(load(file,self))
+        load_cancion(file,self)
     def do_STORE(self,name):
         self.cancion.store(name)
     def do_STEP(self,params=None):
@@ -33,11 +24,11 @@ class Shell(cmd.Cmd):
     def do_TRACKDEL(self,n):
         self.cancion.track_del(convert_num(n))
     def do_MARKADD(self,duracion):
-        self.cancion.mark_add(convert_num(duracion))
+        self.cancion.mark_add(convert_num(duracion,True))
     def do_MARKADDNEXT(self,duracion):
-        self.cancion.mark_add_next(convert_num(duracion))
+        self.cancion.mark_add_next(convert_num(duracion,True))
     def do_MARKADDPREV(self,duracion):
-        self.cancion.mark_add_PREV(convert_num(duracion))
+        self.cancion.mark_add_PREV(convert_num(duracion,True))
     def do_TRACKON(self,n):
         self.cancion.track_on(convert_num(n))
     def do_TRACKOFF(self,n):
@@ -52,3 +43,41 @@ class Shell(cmd.Cmd):
         self.cancion.play_seconds(convert_num(n))
     def actualizar_cancion(self,cancion):
         self.cancion = cancion
+def convert_num(cadena,isfloat=False):
+    """ Convierte una cadena en un int o float segun corresponda. Imprime un mensaje de error si no recibe un numero"""
+    if not cadena.replace('.','').isdigit():
+        print("Ha ingresado un parametro incorrectamente")
+        return
+    if isfloat:
+        return float(cadena)
+    return int(cadena)
+
+def load_cancion(file,shell=None):
+    """Carga la cancion desde el archivo. Reemplaza la cancion en edicion
+    actual si es que la hay.
+    Parametros:
+        file (string) Debe tener el nombre del archivo junto con su extencion (.plp)
+        shell (object) Objeto de la clase Shell para actualizar la cancion"""
+    cancion = Cancion()
+    try:
+        with open(file,"r") as f:
+            for linea in f:
+                campo,valor = linea.rstrip("\n").split(",")
+                if campo == "S":
+                    funcion,frecuencia,volumen = valor.split("|")
+                    cancion.track_add(funcion,convert_num(frecuencia),convert_num(volumen,True))
+                elif campo == "T":
+                    duracion = float(valor)
+                elif campo == "N":
+                    cancion.mark_add(duracion)
+                    posicion = 0
+                    for caracter in valor:
+                        if caracter == "#":
+                            cancion.track_on(posicion)
+                        posicion += 1
+    except IOError as e:
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+        return
+    if shell:
+        shell.actualizar_cancion(cancion) # Actualizamos el atributo del objeto shell
+    print("Cancion cargada con exito")
