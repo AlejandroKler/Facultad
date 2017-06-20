2a8cbef7ebc514903f4e0253ed96d48e5d8bb8c1
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-20 14:25:05 -0300

    nuevos metodos

--- MarcaDeTiempo.py
+++ MarcaDeTiempo.py
@@ -7,4 +7,20 @@ class MarcaDeTiempo():
             duracion (float) Duracion de la marca de tiempo
         """
         self.duracion = duracion
-        self.tracks_habilitados = []
+        self.tracks_habilitados = [] # Almacena los numeros de los tracks habilitados
+
+    def obtener_habilitados(self):
+        """ Devuelve la lista de tracks habilitados"""
+        return self.tracks_habilitados
+
+    def obtener_duracion(self):
+        """ Devuelve la duracion de la marca"""
+        return self.duracion
+
+    def habilitar_track(self,numero_de_track):
+        """Habilita un track"""
+        self.tracks_habilitados.append(numero_de_track)
+
+    def deshabilitar_track(self,numero_de_track):
+        """Deshabilita un track"""
+        self.tracks_habilitados.remove(numero_de_track)
