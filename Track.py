7b156512468c78128d24a199fec0185586a4be83
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-20 14:25:55 -0300

    Nueva clase

---
+++ Track.py
@@ -0,0 +1,19 @@
+class Track():
+    """Representa un track de una cancion"""
+    def __init__(self,tipo,frecuencia,volumen):
+        """Constructor de la clase"""
+        self.tipo = str(tipo)
+        self.frecuencia = int(frecuencia)
+        self.volumen = float(volumen)
+
+    def obtener_tipo(self):
+        """Devuelve el tipo"""
+        return self.tipo
+
+    def obtener_frecuencia(self):
+        """Devuelve la frecuencia"""
+        return self.frecuencia
+
+    def obtener_volumen(self):
+        """Devuelve el volumen"""
+        return self.volumen
