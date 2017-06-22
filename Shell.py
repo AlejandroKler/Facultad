fc63ed4d8b8c8dd76cdcefc6e6858566cbcd8ac5
Alejandro Kler <alejandro_kler@outlook.com>
2017-06-22 17:12:09 -0300

    Version final

--- Shell.py
+++ Shell.py
@@ -4,64 +4,114 @@ import cmd
 
 class Shell(cmd.Cmd):
     intro = "Bienvenido a Sounds of Cyber City.\n Ingrese help o ? para listar los comandos.\n"
-    prompt = "*>> "
+    prompt = "Sounds of Cyber City: "
     cancion = Cancion()
     def do_LOAD(self,file):
-        load_cancion(file,self)
+        """Carga la cancion desde el archivo. Reemplaza la cancion en edicion
+        actual si es que la hay."""
+        print(load_cancion(file,self))
     def do_STORE(self,name):
-        try:
-            self.cancion.store(name)
-        except ValueError as e:
-            print(e.strerror)
+        """Guarda la cancion"""
+        if not name:
+            print("Debe indicar el nombre")
+            return
+        self.cancion.store(name)
     def do_STEP(self,params=None):
+        """Avanza a la siguiente marca de tiempo si la hay. Si no, no hace nada."""
         self.cancion.step()
     def do_STEPM(self,n):
-        self.cancion.stepm(convert_num(n))
+        """Avanza N marcas de tiempo hacia adelante o las que pueda."""
+        if not is_numeric(n):
+            return
+        self.cancion.stepm(int(n))
     def do_BACK(self,params=None):
+        """Retrocede a la anterior marca de tiempo si puede."""
         self.cancion.back()
     def do_BACKM(self,n):
-        self.cancion.load(n)
+        """Retrocede N marcas de tiempo hacia atras o las que pueda."""
+        if not is_numeric(n):
+            return
+        self.cancion.backm(int(n))
     def do_TRACKADD(self,params):
+        """Agrega un track indicandole la funcion, la frecuencia y el volumen (separados por espacios)."""
         lista_parametros = params.split()
         if not len(lista_parametros) == 3:
             print("No ingreso los 3 parametros correctamente")
             return
         funcion,frecuencia,volumen = lista_parametros
-        self.cancion.track_add(funcion,convert_num(frecuencia),convert_num(volumen,True))
-    def do_TRACKDEL(self,n):
-        self.cancion.track_del(convert_num(n))
-    def do_MARKADD(self,duracion):
-        self.cancion.mark_add(convert_num(duracion,True))
-    def do_MARKADDNEXT(self,duracion):
-        self.cancion.mark_add_next(convert_num(duracion,True))
-    def do_MARKADDPREV(self,duracion):
-        self.cancion.mark_add_PREV(convert_num(duracion,True))
-    def do_TRACKON(self,n):
+        if not is_numeric(frecuencia) or not is_numeric(volumen):
+            return
+        if float(volumen) < 0 or 1 < float(volumen):
+            print("El volumen debe estar comprendido entre 0 y 1")
+            return
         try:
-            self.cancion.track_on(convert_num(n))
+            self.cancion.track_add(funcion,int(frecuencia),float(volumen))
+        except ValueError as e:
+            print(e)
+    def do_TRACKDEL(self,n):
+        """Elimina un track por numero"""
+        if not is_numeric(n):
+            return
+        try:
+            self.cancion.track_del(int(n))
+        except IndexError:
+            print('Este track no se encuentra en la canción')
+    def do_MARKADD(self,duracion):
+        """Agrega una marca de tiempo de la duracion indicada."""
+        if not is_numeric(duracion):
+            return
+        self.cancion.mark_add(float(duracion))
+    def do_MARKADDNEXT(self,duracion):
+        """Agrega una marca de tiempo de la duracion indicada en la posicion siguente"""
+        if not is_numeric(duracion):
+            return
+        self.cancion.mark_add_next(float(duracion))
+    def do_MARKADDPREV(self,duracion):
+        """Agrega una marca de tiempo de la duracion indicada en la posicion anterior"""
+        if not is_numeric(duracion):
+            return
+        self.cancion.mark_add_prev(float(duracion))
+    def do_TRACKON(self,n):
+        """Habilita al track durante la marca de tiempo actual"""
+        if not is_numeric(n):
+            return
+        try:
+            self.cancion.track_on(int(n))
         except IndexError:
             print('No existe tal track en la canción')
+        except AttributeError:
+            print("No hay marca")
     def do_TRACKOFF(self,n):
-        self.cancion.track_off(convert_num(n))
+        """Desabilita al track durante la marca de tiempo actual"""
+        if not is_numeric(n):
+            return
+        self.cancion.track_off(int(n))
     def do_PLAY(self,params=None):
+        """Reproduce la marca actual."""
         self.cancion.play()
     def do_PLAYALL(self,params=None):
+        """Reproduce la cancion completa desde el inicio."""
         self.cancion.play_all()
     def do_PLAYMARKS(self,n):
-        self.cancion.play_marks(convert_num(n))
+        """Reproduce las proximas n marcas desde la posicion actual."""
+        if not is_numeric(n):
+            return
+        self.cancion.play_marks(int(n))
     def do_PLAYSECONDS(self,n):
-        self.cancion.play_seconds(convert_num(n))
+        """Reproduce los proximos segundos desde la posicion actual."""
+        if not is_numeric(n):
+            return
+        self.cancion.play_seconds(int(n))
     def actualizar_cancion(self,cancion):
+        """Actualiza la cancion de la Shell"""
         self.cancion = cancion
 
-def convert_num(cadena,isfloat=False):
-    """ Convierte una cadena en un int o float segun corresponda. Imprime un mensaje de error si no recibe un numero"""
+def is_numeric(cadena):
+    """ Devuelve True si recibe una cadena numerica, False en otro caso. Imprime un mensaje de error si no recibe un numero"""
     if not cadena.replace('.','').isdigit():
         print("Ha ingresado un parametro incorrectamente")
-        return
-    if isfloat:
-        return float(cadena)
-    return int(cadena)
+        return False
+    return True
 
 def load_cancion(file,shell=None):
     """Carga la cancion desde el archivo. Reemplaza la cancion en edicion
@@ -77,11 +127,11 @@ def load_cancion(file,shell=None):
                 campo,valor = linea.rstrip("\n").split(",")
                 if campo == "S":
                     funcion,frecuencia,volumen = valor.split("|")
-                    cancion.track_add(funcion,convert_num(frecuencia),convert_num(volumen,True))
+                    cancion.track_add(funcion,int(frecuencia),float(volumen))
                 elif campo == "T":
                     duracion = float(valor)
                 elif campo == "N":
-					#Siempre hay un tipo 'T' antes, donde se define duracion
+                    #Siempre hay un tipo 'T' antes, donde se define duracion
                     if primer_marca:
                         cancion.mark_add(duracion)
                     else:
@@ -92,10 +142,9 @@ def load_cancion(file,shell=None):
                         if caracter == "#":
                             cancion.track_on(posicion)
     except IOError as e:
-        print("I/O error({0}): {1}".format(e.errno, e.strerror))
-        return
+        return "I/O error({0}): {1}".format(e.errno, e.strerror)
     if shell:
         shell.actualizar_cancion(cancion) # Actualizamos el atributo del objeto shell
-    print("Cancion cargada con exito")
+    return "Cancion cargada con exito"
 
 Shell().cmdloop()
