3590dfc7ea4e91b943f28764ac748a9ed03ba862
Alejandro Kler <alejandro_kler@outlook.com>
2017-09-07 13:34:07 -0300

    Update pila.c

--- pila/pila.c
+++ pila/pila.c
@@ -23,7 +23,10 @@ pila_t* pila_crear(void){
 	pila_t* pila = malloc(sizeof(pila_t));
 	if (!pila) return NULL;
 	pila->datos = malloc(sizeof(void));
-	if (!pila->datos) return NULL;
+	if (!pila->datos) {
+		free(pila);
+		return NULL;
+	}
 	pila->capacidad = 1;
 	pila->cantidad = 0;
 	return pila;
@@ -37,14 +40,14 @@ bool pila_esta_vacia(const pila_t *pila){
 }
 bool pila_apilar(pila_t *pila, void* valor){
 	if (pila->cantidad == pila->capacidad){
-		if (!pila_agrandar(pila)) return false;
+		if (!pila_modificar(pila,pila->capacidad*2)) return false; //Incrementamos su capacidad
 	}
 	pila->datos[pila->cantidad] = valor; //Agregamos en el proximo lugar
 	pila->cantidad++;
 	return true
 }
-bool pila_agrandar(pila_t *pila){
-	return !(bool) realloc(pila->datos,sizeof(void) * pila->capacidad * 2);
+bool pila_modificar(pila_t *pila, int tamaño){
+	return !(bool) realloc(pila->datos,sizeof(void) * tamaño);
 }
 void* pila_ver_tope(const pila_t *pila){
 	if (!pila->cantidad) return NULL;
@@ -52,6 +55,9 @@ void* pila_ver_tope(const pila_t *pila){
 }
 void* pila_desapilar(pila_t *pila){
 	if (!pila->cantidad) return NULL;
+	if (pila->cantidad == pila->capacidad/4){
+		if (!pila_modificar(pila,pila->capacidad/2)) return false; //Decrementamos su capacidad
+	}
 	valor = pila->datos[pila->cantidad - 1];
 	pila->cantidad--;
 	return valor;
